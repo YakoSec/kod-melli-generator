933d5315e1d397b458ad5f3d0959579039c5d981
YakoSec <93792838+YakoSec@users.noreply.github.com>
2023-02-10 14:10:05 +0330

    Create melli.py

---
+++ melli.py
@@ -0,0 +1,17 @@
+# Coder = Mr. Yako
+# Instagram = @YakoSec
+# GitHub : @YakoSec
+# Telegram: @YakoSec
+
+import random
+
+import colorama
+from colorama import Fore,Back, Style
+colorama.init()
+
+print(Fore.YELLOW)
+print("Coder: Mr. Yako \nInstagram: @YakoSec \nGitHub: @YakoSec \nTelegram: @YakoSec")
+
+x = random.randint(1000000000, 100000000000)
+print(Fore.RED)
+print("تسا رضاح امش یلم دک: " , x)
